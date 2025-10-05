38977b799736cf6eca318b5e92fb532ed77a6f4b
liyanda <litoncpt025@student.wethinkcode.co.za>
2025-10-05 09:14:19 +0200

    First Commit

---
+++ helloworld.py
@@ -0,0 +1,26 @@
+# Introduction
+# Day 1 
+
+
+print(3 + 2) # addition (+)
+print(3 - 2) # subtraction (-)
+print(3 * 2) # multiplication (*)
+print(3 / 2) # division (/)
+print(3 ** 2) # exponential (**)
+print(3 % 2) # modulus (%)
+print(3 // 2) # floor division operator (//)
+
+
+''' DATA TYPES '''
+
+
+print(type(10)) # Int
+print(type(3.14)) # Float 
+print(type(1 + 3j)) # Complex
+print(type('Inkredible')) # String
+print(type([1, 2, 3])) # List
+print(type({'name': 'Inkredible'})) # Dictionary
+print(type({9.8, 3.14, 2.7})) # Set
+print(type(9.8, 3.14, 2.7)) # Tuple
+print(type(3 == 3)) # Bool 
+print(type(3 >= 3)) # Bool
