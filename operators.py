cd33c660b9ba23b31afd806131ddf73f115305d3
liyanda <litoncpt025@student.wethinkcode.co.za>
2025-10-26 21:03:54 +0200

    Third Commit

---
+++ operators.py
@@ -0,0 +1,18 @@
+# Arithmetic Operators in Python 
+# Integers 
+
+print(1 + 2) # Addition
+print(2 - 2) # Subtraction 
+print(2 * 3) # Multiplication
+print(4 / 2) # division that gives floating number
+print(6 / 2) # Division 
+print(7 / 2) # Division
+print(7 // 2) # Division w/o remainder
+print(3 % 2) # Modulus 
+print(7 // 3) # Division w/o remainder
+print(3 ** 3) # Exponential
+
+print(1 + 1j) # Complex numbers 
+print((1 + 1j) * (1 - 1j)) # Multiplying complex numbers
+
+
