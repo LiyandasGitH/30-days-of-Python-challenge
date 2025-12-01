50ccf4d74c037cad7939156ce68e2f7be84d42c2
liyanda <litoncpt025@student.wethinkcode.co.za>
2025-12-01 23:32:22 +0200

    more practie

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,20 @@
 def main():
     print(is_palindrome("hello"))
+    print(is_palindrome("racecar"))
     
 def is_palindrome(string) -> bool:
-    ...
-
+    return string == string[::-1]
 
 if __name__ == "__main__":
     main()
+
+
+# def is_palindrome(string:str) -> bool:
+#     left, right = 0, len(string) - 1
+    
+#     while left < right:
+#         if string[left] != string[right]:
+#             return False
+#         left += 1
+#         right -= 1
+#     return True
---
+++ sum.py
@@ -0,0 +1,5 @@
+def sum_of_numbers(n):
+    total = 0
+    for num in range(1, n + 1):
+        total += num
+    return total
